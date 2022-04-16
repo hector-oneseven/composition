71e0a89820bf04e5bdf52a53afaeff1c7e7764c6
Hector Ibarra <>
2022-04-15 21:26:37 -0400

    componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import {TodoCounter} from './TodoCounter'
+import {TodoSearch} from './TodoSearch'
+import {TodoList} from './TodoList'
+import {TodoItem} from './TodoItem'
+import {CreateTodoButton} from './CreateTodoButton'
 // import './App.css';
 const todos = [
   {text: 'Cortar cebolla', completed: false},
-  {text: 'Cortar cebolla1', completed: false},
-  {text: 'Cortar cebolla2', completed: false}
+  {text: 'Cortar cebolla', completed: false},
+  {text: 'Cortar cebolla', completed: false}
 ]
 function App(props) {
   return (
   <React.Fragment>
     <TodoCounter/>
-    {/* <TodoSearch/> */}
-    <input placeholder="Cebolla" />
-    {/* <TodoList>
+    <TodoSearch/>
+    <TodoList>
       {todos.map(todo => (
-        <TodoItem/>
+        <TodoItem key={todo.index} text={todo.text}/>
       ))}
-    </TodoList> */}
-    {/* <CreateTodoButton/> */}
-    <button>+</button>
+    </TodoList>
+    <CreateTodoButton/>
+    
   </React.Fragment>
    
   );
